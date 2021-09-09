02d38bfc38329259fb82ca751af4201a5111316e
Peter Ahrens <ptrahrens@gmail.com>
2021-09-09 13:38:09 -0400

    It might make asense to split the asymptotes into a new package

--- test/test_asymptote_equal.jl
+++ test/test_asymptote_equal.jl
@@ -3,5 +3,8 @@
     a = Such(Times(:i, :j, :k), Wedge(Predicate(:A, :i, :j)))
     b = Cup(Such(Times(:i, :j, :k), Vee(Wedge(Predicate(:A, :i, :j), true), false)), Empty())
     @test Pigeon.asymptote_equal(a, b)
+
+    a = Such(Times(:i, :j, :k), Wedge(Predicate(:A, :i, :j)))
+    b = Cup(Such(Times(:i, :j, :k), Vee(Wedge(Predicate(:A, :i, :j), true), false)), Empty())
     @test Pigeon.asymptote_equal(a, b)
 end

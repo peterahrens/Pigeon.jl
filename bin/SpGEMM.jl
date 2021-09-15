7df75c34826cdbf0af1dcaa80aac7014f8ca8fca
Peter Ahrens <ptrahrens@gmail.com>
2021-09-15 14:41:03 -0400

    okay, time to fix up lower_asymptote

--- bin/SpGEMM.jl
+++ bin/SpGEMM.jl
@@ -6,13 +6,8 @@ C = Fiber(:C, [coiter, coiter], [:K, :J])
 
 ex = i"∀ k, j, i A[i,j] += B[i, k] * C[k, j]"
 
-display(asymptote(ex))
-display(simplify_asymptote(asymptote(ex)))
-
-#=
 schedules = saturate_index(ex)
 
 frontier = filter_pareto(schedules)
 
 foreach(display, frontier)
-=#

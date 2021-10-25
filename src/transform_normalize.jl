8dd6c3140cee5aded75d1c53f226fff9032d4322
Peter Ahrens <ptrahrens@gmail.com>
2021-10-25 19:29:03 -0400

    don't sort

--- src/transform_normalize.jl
+++ src/transform_normalize.jl
@@ -7,7 +7,7 @@ normalize_index = Fixpoint(Postwalk(Chain([
 	(@ex@rule @i(*(~~a, *(~~b), ~~c)) => @i *(~~a, ~~b, ~~c)),
 	(@ex@rule @i(*(~a)) => ~a),
 	(@ex@rule @i(*(~~a, - ~b, ~~c)) => @i -(*(~~a, ~b, ~~c))),
-	(@ex@rule @i(+(~~a)) => if !issorted(~~a) @i +($(sort(~~a))) end),
-	(@ex@rule @i(*(~~a)) => if !issorted(~~a) @i *($(sort(~~a))) end),
+	#(@ex@rule @i(+(~~a)) => if !issorted(~~a) @i +($(sort(~~a))) end),
+	#(@ex@rule @i(*(~~a)) => if !issorted(~~a) @i *($(sort(~~a))) end),
 	(@ex@rule @i(@loop ~~i @loop ~~j ~s) => @i @loop ~~i ~~j ~s),
 ])))

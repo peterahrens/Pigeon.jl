8c510dedcf4c803557727fec4d7397f08cf986b4
Peter Ahrens <ptrahrens@gmail.com>
2021-11-10 19:16:40 -0500

    wow that was a bad SymbolicUtils update.

--- src/Pigeon.jl
+++ src/Pigeon.jl
@@ -33,8 +33,6 @@ export filter_pareto
 export simplify_asymptote
 export supersimplify_asymptote
 
-TermInterface.exprhead(::Any) = :call 
-
 include("utils.jl")
 include("index.jl")
 include("parse_index.jl")
